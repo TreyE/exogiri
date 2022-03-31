d4c5357c2dfc8ad04efe7ce84a9ec055d3d8c4a3
Trey Evans <lewis.r.evans@gmail.com>
2022-03-31 17:21:57 -0400

    Trying to find libraries on ubuntu.

--- bundlex.exs
+++ bundlex.exs
@@ -7,6 +7,16 @@ defmodule Exogiri.BundlexProject do
     ]
   end
 
+  def natives(:linux) do
+    [
+      exogiri_xml_internal: [
+        sources: ["xml/exogiri.c", "xml/document.c", "xml/node.c", "xml/xpath.c", "xml/errors.c", "xml/utils.c", "xml/schema.c"],
+        includes: ["/include", "/usr/include", "/usr/include/libxml2"],
+        interface: :nif
+      ]
+    ]
+  end
+
   def natives(_) do
     [
       exogiri_xml_internal: [
