7512f7533288c31430fed2ade597d837a0b30a75
Trey Evans <lewis.r.evans@gmail.com>
2022-03-31 16:30:51 -0400

    Cleaner documentation.

--- lib/exogiri/xml/schema.ex
+++ lib/exogiri/xml/schema.ex
@@ -10,23 +10,32 @@ defmodule Exogiri.Xml.Schema do
 
   @doc """
   Read a schema from a string.
+
+  As you don't have a path, this doesn't handle more complex cases like
+  included or imported schemas, if either is local.
   """
-  @spec from_string(String.t) :: {:error, :parse_failed | [String.t]} | {:ok, t}
+  @spec from_string(String.t) :: {:error, :unknown_error | [String.t]} | {:ok, t}
   def from_string(string) do
     case Exogiri.Xml.Internal.priv_schema_from_string(string) do
-      {:error, :parse_failed} -> {:error, :parse_failed}
+      {:error, :unknown_error} -> {:error, :unknown_error}
       {:error, errs} -> {:error, format_errors(errs)}
       {:ok, a} -> {:ok, %__MODULE__{ref: a}}
     end
   end
 
   @doc """
-  Read a schema from a with a base path.
+  Read a schema from a string with a path.
+
+  This version handles local includes and imports correctly.  The path argument
+  is the location of the actual schema you are parsing as a string, NOT its
+  directory.  The schema isn't read from the path, so it doesn't have to be
+  real - the path is used to establish the base location for any local schema
+  references.
   """
-  @spec from_string(String.t, String.t) :: {:error, :parse_failed | [String.t]} | {:ok, t}
+  @spec from_string(String.t, String.t) :: {:error, :unknown_error | [String.t]} | {:ok, t}
   def from_string(string, path) do
     case Exogiri.Xml.Internal.priv_schema_from_string_with_path(string, path) do
-      {:error, :parse_failed} -> {:error, :parse_failed}
+      {:error, :unknown_error} -> {:error, :unknown_error}
       {:error, errs} -> {:error, format_errors(errs)}
       {:ok, a} -> {:ok, %__MODULE__{ref: a}}
     end
