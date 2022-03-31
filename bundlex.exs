6459870f98b1963b19e98aefc432cf7dc5160ca0
Trey Evans <lewis.r.evans@gmail.com>
2022-03-31 17:29:56 -0400

    Trying to get compilation.

--- bundlex.exs
+++ bundlex.exs
@@ -7,22 +7,14 @@ defmodule Exogiri.BundlexProject do
     ]
   end
 
-  def natives(:linux) do
+  def natives(_) do
     [
       exogiri_xml_internal: [
         sources: ["xml/exogiri.c", "xml/document.c", "xml/node.c", "xml/xpath.c", "xml/errors.c", "xml/utils.c", "xml/schema.c"],
-        includes: ["/include", "/usr/include", "/usr/include/libxml2"],
+        includes: ["/include/", "/usr/include/", "/usr/include/libxml2/"],
         interface: :nif
       ]
     ]
   end
 
-  def natives(_) do
-    [
-      exogiri_xml_internal: [
-        sources: ["xml/exogiri.c", "xml/document.c", "xml/node.c", "xml/xpath.c", "xml/errors.c", "xml/utils.c", "xml/schema.c"],
-        interface: :nif
-      ]
-    ]
-  end
 end
